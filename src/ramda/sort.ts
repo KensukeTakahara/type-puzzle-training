8332f17605d5dd2f6e3af07222359c27dac27f84
Kensuke Takahara <zawcjk@gmail.com>
2019-07-19 23:49:32 +0900

    improve sort.ts.

--- src/ramda/sort.ts
+++ src/ramda/sort.ts
@@ -17,14 +17,14 @@ const propsSample = R.props(['number', 'name'], sampleRoom)
 
 const sampleRooms: Room[] = [
   {
-    floor: 2,
+    floor: 3,
     number: 2,
-    name: '202'
+    name: '302'
   },
   {
-    floor: 2,
+    floor: 1,
     number: 3,
-    name: '203'
+    name: '103'
   },
   {
     floor: 2,
@@ -33,12 +33,20 @@ const sampleRooms: Room[] = [
   }
 ]
 
-const sortedRooms = R.sortBy(R.prop('number'))(sampleRooms)
+const sortedByNumberRooms = R.sort((a: Room, b: Room) => a.number - b.number, sampleRooms)
 /*
 [ { floor: 2, number: 1, name: '201' },
-  { floor: 2, number: 2, name: '202' },
-  { floor: 2, number: 3, name: '203' } ]
+  { floor: 3, number: 2, name: '302' },
+  { floor: 1, number: 3, name: '103' } ]
 */
-console.log(sortedRooms)
+console.log(sortedByNumberRooms)
+
+const sortedByFloorRooms = R.sortBy(R.prop('floor'))(sampleRooms)
+/*
+[ { floor: 1, number: 3, name: '103' },
+  { floor: 2, number: 1, name: '201' },
+  { floor: 3, number: 2, name: '302' } ]
+*/
+console.log(sortedByFloorRooms)
 // false not mutate
-console.log(sampleRooms === sortedRooms)
+console.log(sampleRooms === sortedByFloorRooms)
