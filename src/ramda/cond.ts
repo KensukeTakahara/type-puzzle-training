acfd5d3d02dea7704ec73c8506459deb65980b11
Kensuke Takahara <zawcjk@gmail.com>
2019-07-26 00:22:29 +0900

    add default case to cond.ts.

--- src/ramda/cond.ts
+++ src/ramda/cond.ts
@@ -10,9 +10,11 @@ const predType = (ramdaType: string) => (type: unknown) => R.type(type) === ramd
 const f = R.cond([
   [predType('String'), R.toUpper],
   [predType('Number'), R.multiply(1000)],
-  [predType('Boolean'), R.not]
+  [predType('Boolean'), R.not],
+  [R.T, R.always('not string & number & boolean.')]
 ])
 
 console.log(f('hello')) // => HELLO
 console.log(f(0.01)) // => 10
 console.log(f(true)) // => false
+console.log(f([])) // => not string & number & boolean.
